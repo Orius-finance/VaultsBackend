780049350f02c6de89932a1038815b428133922c
user <sergioreklamplay@gmail.com>
2025-02-02 16:05:27 +0200

    feat: empty placholders views and models for models

--- vaults/models.py
+++ vaults/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Vault(models.Model):
+    vault_address = models.CharField(max_length=42)
+    accountant_address = models.CharField(max_length=42)
--- vaults/views.py
+++ vaults/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
-class
+
+class VaultsDetail(viewsets.ViewSet):
+    def get(self, request):
+        pass
+
+    @action("tvl", detail=True)
+    def get_tvl(self, request):
+        pass
